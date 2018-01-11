91f373b90c78f499d046f5bfe214c7a1bb40755a
dcvasquez1 <dcvasquez@outlook.com>
2018-01-11 10:13:59 -0800

    post

--- webapp.py
+++ webapp.py
@@ -6,18 +6,26 @@ app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  
 def render_main():
     return render_template('home.html')
 
-@app.route("/response")
+@app.route("/response", methods=['GET', 'POST')
 def render_response():
     color = request.args['color']
     #The request object stores infor about the request sent to the server
     #args is a "MultiDict" - a dictionary that can hold more than one value per key
     #The information in args is visible in the url for the page being requested (ex. .../response?color=blue)
-
-    if color == "blue":
-        reply = "Nice! That's my favorite color, too!"
+    
+    error = None
+    if request.method == 'POST':
+        if color == "blue":
+            reply = "Nice! That's my favorite color, too!"
+        else:
+            reply = "Are you kidding me, " + color + "?! What a garbo color. Blue is clearly the best color"
+         return render_template('response.html', response = reply, error=error)
     else:
-        reply = "Are you kidding me, " + color + "?! What a garbo color. Blue is clearly the best color"
-    return render_template('response.html', response = reply)
+        error = 'color invalid'
+        return render_template('response.html', error=error)
+   
+def login():
+    
     
 if __name__=="__main__":
     app.run(debug=False, port=54321)
